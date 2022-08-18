0597d75cd15191f4fd6b7c93d41d1f1f63733e89
samsulai <samuelsulai2015@gmail.com>
2022-08-18 10:26:08 +0100

    added

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ function Header() {
         }
 	 }, [])
 	return (
-		<header className={`${isScrolled && 'bg-[#141414]'}`}>
+		<header className={` ${isScrolled && 'bg-[#141414]'}`} >
 			<div className="flex items-center space-x-2 md:space-x-10">
 			<img src="https://rb.gy/ulxxee" width={100} height={100} className="cursor-pointer object-contain" />
 			<ul className="hidden md:flex space-x-4">
