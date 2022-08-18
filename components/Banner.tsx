a952ce88ba65fbba81f27d93ea4de6f457194615
samsulai <samuelsulai2015@gmail.com>
2022-08-18 18:44:11 +0100

    added

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,7 +22,7 @@ function Banner({netflixOriginals}: Props) {
 			<Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} layout="fill" objectFit="cover"/>
 			</div>
 			<h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">{movie?.title || movie?.name || movie?.original_name}</h1>
-			<p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+			<p className="max-w-xs text-xs text-white text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
 		<div className="flex space-x-3">
          <button className="flex items-center gap-x-2  rounded px-5 py-1.5 bg-white text-black text-sm font-semibold transition hover:opacity-75 md:py-2.5 md:px-8 md:text-xl;">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7 " />
