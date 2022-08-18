5891649226040d40611398a157fd6b97fc49c158
samsulai <samuelsulai2015@gmail.com>
2022-08-18 12:30:42 +0100

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,6 +32,7 @@ const Home = ({netflixOriginals,
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
       </Head>
       <Header />
        {/*header*/}
