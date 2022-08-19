40e1f5b789154ca83be741cf3cfe1f471fabcb94
samsulai <samuelsulai2015@gmail.com>
2022-08-19 09:22:27 +0100

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,9 @@
 module.exports = {
-  purge : {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-  ]},
+  ],
   theme: {
-     screens: {
-      'sm': '400px',
-     
-      'md': '768px',
-      
-
-      'lg': '976px',
-      
-
-      'xl': '1440px',
-      'ds': '1920px',
-      'dm': '2480px',
-      'dl': '2700px',
-      // => @media (min-width: 1280px) { ... }
-    },
     extend: {
       textShadow: {
         md: '2px 2px 4px rgb(0 0 0 / 45%);',
