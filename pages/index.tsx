515ed876a511f5463fe977593a85c0f4a9ada3ae
samsulai <samuelsulai2015@gmail.com>
2022-08-19 14:22:23 +0100

    fff

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home = ({netflixOriginals,
   } : Props) => {
   console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-black-500 to-black-500  lg:h-[140vh] !scrollbar-thin  !scrollbar-thumb-red-600 ">
+    <div className="relative h-screen bg-gradient-to-b from-black-500 lg:h-[140vh] !scrollbar-thin  !scrollbar-thumb-red-600 ">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
