a0a49613edb933fce8b4879fa53519cd308c1202
samsulai <samuelsulai2015@gmail.com>
2022-08-19 14:47:20 +0100

    add

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ function Header() {
 			<div className="flex items-center space-x-4 text-sm font-light">
 			<SearchIcon className="hidden h-6 w-6 sm:inline"/>
 			<p className="hidden lg:inline">Kids</p>
-			<BellIcon className="h-6 w-6 fill-white-500"/>
+			<BellIcon className="h-6 w-6 fill-[#ffffff]"/>
 			<Link href="/account">
 			<img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded" />
 			</Link>
